8273706d91315c61e593e0a1592436044139d904
athena909 <altameranomay@yahoo.com.ph>
2025-08-08 07:01:47 +0800

    modified navbar

--- src/content/HamburgerMenu.js
+++ src/content/HamburgerMenu.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
+const imgLogo = '/images/Jefbel_logo.png';
+
 const HamburgerMenu = () => {
   return (
     <Navbar id="navbar" expand="lg" bg="light" variant="light">
       <Container>
         <Navbar.Brand href="/" className='-mobile'>
-          <img src={process.env.PUBLIC_URL + '/images/Jefbel_logo.png'} alt="jeffbell logo"/>
+          <img src={process.env.PUBLIC_URL + imgLogo} alt="wedding logo"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* Hamburger icon */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#" onclick="window.location.reload(true);">RSVP</Nav.Link>
-            <Nav.Link href="/">OUR STORY</Nav.Link>
+            <Nav.Link href="#" onclick="window.location.reload(true);">OUR STORY</Nav.Link>
+            <Nav.Link href="#" onclick="window.location.reload(true);">ITINERARY</Nav.Link>
             
-            <Nav.Link href="/">LOCATION</Nav.Link>
             
           
             <Navbar.Brand href="/">
-              <img src={process.env.PUBLIC_URL + '/images/Jefbel_logo.png'} alt="jeffbell logo"/>
+              <img src={process.env.PUBLIC_URL + imgLogo} alt="jeffbell logo"/>
             </Navbar.Brand>
 
-            <Nav.Link href="/">ITINERARY</Nav.Link>
-            <Nav.Link href="/">DRESS CODE</Nav.Link>
-            <Nav.Link href="/">GALLERY</Nav.Link>
+            
+            <Nav.Link href="#" onclick="window.location.reload(true);">DRESS CODE</Nav.Link>
+            <Nav.Link href="#" onclick="window.location.reload(true);">LOCATION</Nav.Link>
+            <Nav.Link href="#" onclick="window.location.reload(true);">GALLERY</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
